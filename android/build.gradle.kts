442e6d5cbb368783e016d1eb3c8f0d5651a09f2b
rezagasem1357 <rezagasem.82@gmail.com>
2026-09-02 19:12:22 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,4 @@
 buildscript {
-    ext {
-        kotlin_version = "1.9.22"
-    }
     repositories {
         google()
         mavenCentral()
@@ -9,7 +6,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
