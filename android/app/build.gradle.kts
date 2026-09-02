d14165b4c03003c6824a2ea1a50c33f4ff3ba47a
rezagasem1357 <rezagasem.82@gmail.com>
2026-09-02 18:33:05 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,66 +1,43 @@
-def localProperties = new Properties()
-def localPropertiesFile = rootProject.file('local.properties')
-if (localPropertiesFile.exists()) {
-    localPropertiesFile.withReader('UTF-8') { reader ->
-        localProperties.load(reader)
-    }
-}
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 
-def flutterRoot = localProperties.getProperty('flutter.sdk')
-if (flutterRoot == null) {
-    throw new GradleException("Flutter SDK not found. Define location with flutter.sdk in the local.properties file.")
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
 }
-
-def flutterVersionCode = localProperties.getProperty('flutter.versionCode')
-if (flutterVersionCode == null) {
-    flutterVersionCode = '1'
-}
-
-def flutterVersionName = localProperties.getProperty('flutter.versionName')
-if (flutterVersionName == null) {
-    flutterVersionName = '1.0'
-}
-
-apply plugin: 'com.android.application'
-apply plugin: 'kotlin-android'
-apply from: "$flutterRoot/packages/flutter_tools/gradle/flutter.gradle"
 
 android {
-    namespace "com.example.last_version_test"  // ✅ نام پکیج خود را وارد کنید
-    compileSdk 36  // ✅ از 34 به 36 تغییر دهید
-    
+    namespace = "com.example.last_version_test"  // ✅ نام پکیج خود را وارد کنید
+    compileSdk = 36  // ✅ SDK 36
+
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = '17'
-    }
-
-    sourceSets {
-        main.java.srcDirs += 'src/main/kotlin'
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        applicationId "com.example.last_version_test"  // ✅ نام پکیج خود را وارد کنید
-        minSdk 21
-        targetSdk 36  // ✅ از 34 به 36 تغییر دهید
-        versionCode flutterVersionCode.toInteger()
-        versionName flutterVersionName
+        applicationId = "com.example.last_version_test"  // ✅ نام پکیج خود را وارد کنید
+        minSdk = 21
+        targetSdk = 36  // ✅ SDK 36
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
-        release {
-            signingConfig signingConfigs.debug
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 flutter {
-    source '../..'
+    source = "../.."
 }
 
 dependencies {
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version")
 }
